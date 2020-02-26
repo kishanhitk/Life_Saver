0c0a9b3831012e4bc451db43979ed31a893831f6
kishanhitk <kishansharma1231@gmail.com>
2020-02-26 23:33:07 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bank/screens/landingpage/landingpage.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Quicksand').copyWith(
+        
+        
+        primaryColor: Color(0xFF2D78FF),
+      ),
+      home: LandingPage(),
+    );
+  }
+}
---
+++ lib/screens/landingpage/landingpage.dart
@@ -0,0 +1,101 @@
+import 'package:bank/screens/donorlogin/donorloginpage.dart';
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Welcome to BloodBank",
+          style:
+              TextStyle(fontWeight: FontWeight.w300, fontFamily: 'Quicksand'),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+       
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+        
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Text(
+                "I am a,",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          SelectYou(
+            text: "Donor",
+          ),
+          SelectYou(
+            text: "Patient",
+          ),
+          SelectYou(
+            text: "Hospital",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SelectYou extends StatelessWidget {
+  String text;
+  SelectYou({this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => DonorLoginPage()));
+        },
+        child: Padding(
+    padding: const EdgeInsets.symmetric(horizontal:18.0,vertical: 7),
+    child: Material(
+      color: Colors.transparent,
+      elevation: 5,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [Colors.blue[700], Colors.blue[500]]),
+          borderRadius: BorderRadius.circular(10),
+          color: Color(0xFF2D78FF),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 18.0),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 33,
+                  fontFamily: 'Quicksand'),
+            ),
+          ),
+        ),
+      ),
+    ),
+        ),
+      );
+  }
+}
